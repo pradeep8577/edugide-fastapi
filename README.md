63ae6848df6a1d3060ab90545165eb114a799f92
pradeep8577 <pradeeppawar8577@gmail.com>
2024-03-30 14:30:06 +0530

    add: new

--- README.md
+++ README.md
@@ -1,22 +1,31 @@
-# Chat With Multiple PDFs Using Gemini 
-
-# AIM
-The project aims to use Generative AI for providing answers to questions asked by user after the model studies through the multiple input PDFs.  
-
-## Project Link:
-Feel free to check out the project: https://chat-with-pdf-nitya.streamlit.app/
-
-# Working and Technologies
-The model first takes in the PDFs using FAISS Library and creating Pickle files. It then generates answers to question through the pickle file. If the question asked by user is out of scope with respect to the documents provided, it outputs a statement stating that the question is out of context.
-## Technologies Used
-- Python
-- Gemini API
-- FAISS Library
-- Streamlit
-- Langchain
-- PyPDF2 Library
-
-# Interface Snippets
-![image](https://github.com/nityakapur/chat-with-pdf/assets/124587467/d1a8f626-058f-40a4-90cb-9673a231d709)
+# Gemini Pro Chatbot with Streamlit
 
 
+https://github.com/koji/GeminiPro-Streamlit-ChatBot/assets/474225/5a2c807c-51c0-41ba-99ee-31c6ad968a69
+
+
+requirements
+- google-generativeai
+- streamlit (if you want to run the code on your local machine)
+- google gemini api key (https://makersuite.google.com/app/apikey)
+
+This repo's code is running the streamlit on HuggingFace space.
+ 
+## how to setup HuggingFace space
+1. Create a new space  
+2. Add secret variable  
+In this code the variable name is `GOOGLE_GEMINI_KEY`. You can name whatever you want to name. If you change the name, you need to update the name in `app.py`.  
+3. Push the code to your HuggingFace space  
+
+
+## how to run locally
+If you want to run the code locally, you can do that. Before running, please make sure that you install `streamlit` and `google-generativeai` on your machine.
+
+```zsh
+pip install streamlit google-generativeai
+```
+
+Then run the following command. You will see 2 urls for the streamlit application.
+```zsh
+streamlit run app.py
+```
--- app.py
+++ app.py
@@ -1,93 +1,30 @@
-import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import streamlit as st
 import google.generativeai as genai
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
-from langchain_community.vectorstores import FAISS
 
-load_dotenv()
-#os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+try:
+    gemini_api_key = os.environ['GOOGLE_GEMINI_KEY']
+    genai.configure(api_key=gemini_api_key)
+    model = genai.GenerativeModel('gemini-pro')
 
+    if "chat" not in st.session_state:
+      st.session_state.chat = model.start_chat(history=[])
+    st.title('Gemini Pro Test')
 
-def get_pdf_text(pdf_docs):
-    text=""
-    for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text+= page.extract_text()
-    return  text
+    def role_to_streamlit(role: str) -> str:
+      if role == 'model':
+        return 'assistant'
+      else:
+        return role
 
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks = text_splitter.split_text(text)
-    return chunks
+    for message in st.session_state.chat.history:
+      with st.chat_message(role_to_streamlit(message.role)):
+        st.markdown(message.parts[0].text)
 
-def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
-
-def get_conversational_chain():
-
-    prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
-
-    Answer:
-    """
-
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
-                             temperature=0.3)
-
-    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-
-    return chain
-
-
-def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    
-    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
-    docs = new_db.similarity_search(user_question)
-
-    chain = get_conversational_chain()
-
-    
-    response = chain(
-        {"input_documents":docs, "question": user_question}
-        , return_only_outputs=True)
-
-    print(response)
-    st.write("Reply: ", response["output_text"])
-
-def main():
-    st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
-
-    user_question = st.text_input("Ask a Question from the PDF Files")
-
-    if user_question:
-        user_input(user_question)
-
-    with st.sidebar:
-        st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Submit & Process"):
-            with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
-                st.success("Done")
-
-
-if __name__ == "__main__":
-    main()
+    if prompt := st.chat_input("I possess a well of knowledge. What would you like to know?"):
+      st.chat_message("user").markdown(prompt)
+      response = st.session_state.chat.send_message(prompt)
+      with st.chat_message("assistant"):
+        st.markdown(response.text)
+except Exception as e:
+    st.error(f'An error occurred: {e}')
